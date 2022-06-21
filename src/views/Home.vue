6644417d8103857ee5a610aa8e0bdbc2e2c2544b
Caio <bladellano@gmail.com>
2022-06-21 08:46:27 -0300

    Estilizando paginas

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <h1>Home</h1>
+  </div>
 </template>
 
 <script>
 export default {
+  // eslint-disable-next-line vue/multi-word-component-names
   name: "Home",
 };
 </script>
+
+<style>
+</style>
